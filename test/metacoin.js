d88fc27346a23291842fb58de6fae2d5b23d67d3
syrohei <syrohei@gmail.com>
2016-07-18 15:29:29 +0900

    Fix count => delay_time

--- test/metacoin.js
+++ test/metacoin.js
@@ -1,45 +1,43 @@
 
 const value = 1200
+const delay_time = 4
 
 contract('MetaCoin', function(accounts) {
-  it("should put 10000 MetaCoin in the first account", function(done) {
+  it("should no error of hyper sending transactions with delay", function(done) {
 
     this.timeout(120000000);
     const  meta = MetaCoin.deployed();
 
-    const  array = createArray(value) 
-    const  mined = new Array(0) 
-    const  back = new Array(0) 
+    const  array = createArray(value)
+    const  mined = new Array(0)
+    const  back = new Array(0)
 
     const length = array.length
-    /*
-    sync(meta, function(count){
-      console.log(count, error)
-    })
-    */
-    async(array, 0, mined, back, meta, function(resend){
+
+    delay(array, 0, mined, back, meta, function(resend){
       if (!resend.length)
         done()
 
       console.log(`first step error count is ${Number(resend.length)}`)
       console.log(`second step is started...`)
-      async(resend, 0, [],[], meta, function(resend){
+      delay(resend, 0, [],[], meta, function(resend){
         if (!resend.length)
           done()
 
         console.log(`second step error count is ${resend.length}`)
         console.log(`third step is started...`)
-        async(resend, 0, [], [],  meta, function(resend){
+        delay(resend, 0, [], [],  meta, function(resend){
           console.log(`third step error count is ${resend.length}`)
           done()
         })
       })
     })
+
   })
 });
 
 function createArray(count){
-  const array = new Array(count) 
+  const array = new Array(count)
   const list = []
   count = 1
   for (let i of array) {
@@ -53,24 +51,8 @@ function createArray(count){
 }
 
 
-function sync(array, meta, cb){
-  const dats = String(Math.random().toString(36).slice(-12));
-  const data = dats.concat(String(Math.random().toString(36).slice(-20)))
-  const amount = Math.round(Math.random()*100)
-  meta.register(String(data), Number(amount)).then(function(tx) {
-    meta.balances(String(data)).then(function(balance) {
-      count++ 
-      console.log(tx,String(data), amount, balance.toNumber(), count)
-      if (count > max)
-        return cb(count)
 
-      put(meta,cb)
-    })
-  })
-}
-
-
-function async(array, max, mined, back, meta, cb){
+function delay(array, max, mined, back, meta, cb){
 
   if (!array[0])
     return 0
@@ -79,8 +61,8 @@ function async(array, max, mined, back, meta, cb){
     max = array.length
 
   const v = array.shift()
-  
-  
+
+
   meta.register(String(v.data), Number(v.amount)).then(function(tx) {
     //if transactions were mined, push the transaction to array
     meta.balances(String(v.data)).then(function(balance) {
@@ -100,9 +82,6 @@ function async(array, max, mined, back, meta, cb){
     array.push(v)
   })
   setTimeout(function(){
-    async(array, max,mined, back, meta, cb)
-  },1)
+    delay(array, max,mined, back, meta, cb)
+  }, delay_time)
 }
-
-
-
